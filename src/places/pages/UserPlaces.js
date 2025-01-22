f786e5f5aa54197cf8115679072b0f7087a5a6f1
Hariharan <hariharan17kpm@gmail.com>
2025-01-22 11:54:33 +0530

    Map added

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -12,7 +12,7 @@ const DUMMY_PLACES = [
     address: "20 W 34th St., New York, NY 10001, United States",
     location: {
       lat: 40.7484405,
-      lon: -73.9856644,
+      lng: -73.9856644,
     },
     creator: "u1",
   },
@@ -25,7 +25,7 @@ const DUMMY_PLACES = [
     address: "20 W 34th St., New York, NY 10001, United States",
     location: {
       lat: 40.7484405,
-      lon: -73.9856644,
+      lng: -73.9856644,
     },
     creator: "u2",
   },
